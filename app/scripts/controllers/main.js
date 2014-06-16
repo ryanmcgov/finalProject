a5d82d6bfb1e8536e5e709ede3b7e84efd215fb1
Ryan McGovern <ryanmcgovern@Ryans-MacBook-Air.local>
2014-06-16 14:10:33 -0400

    working sidebar

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -178,6 +178,24 @@ angular.module('whatNext')
       	}
     ];
   })
+  .controller('DateCtrl', function ($scope) {
+      $scope.today = function() {
+        $scope.dt = new Date();
+      };
+      $scope.today();
+
+      $scope.clear = function () {
+        $scope.dt = null;
+      };
+
+      $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+      };
+
+  })
   .controller('CollapseCtrl', function ($scope){
       $scope.isCollapsed = false;
   })
